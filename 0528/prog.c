85478e9e153194df7bebfbb5ae1928c422855e11
stonericefield <89579455+stellarcielo@users.noreply.github.com>
2025-05-28 09:26:13 +0900

    Add files via upload

---
+++ 0528/prog.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <pigpiod_if2.h>
+
+#define HIGH 1
+#define LOW 0
+
+#define LEDPIN 17
+
+#define LOOPCOUNT 100
+
+int main(int argc, char *argv[]){
+	
+	if(argc != 2){
+		fprintf(stderr, "Usage: %s <num>\n", argv[0]);
+		fprintf(stderr, "num: light level 0~9\n");
+		exit(1);
+	}
+	int level = atoi(argv[1]);
+
+	if(0 > level && level < 9){
+		fprintf(stderr, "Level is 0~9.");
+		exit(1);
+	}
+
+	int pd, t;
+
+	if ((pd = pigpio_start(NULL, NULL)) < 0){
+		printf("pigpio connection failed.\n");
+		printf("pigpio check start.\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	set_mode(pd, LEDPIN, PI_OUTPUT);
+
+	t = 0;
+
+	while(t < LOOPCOUNT){
+		gpio_write(pd, LEDPIN, HIGH);
+		time_sleep(level * 0.001);
+		gpio_write(pd, LEDPIN, LOW);
+		time_sleep(0.01 - level * 0.001);
+		t++;
+	}
+	
+	pigpio_stop(pd);
+
+	return 0;
+}
